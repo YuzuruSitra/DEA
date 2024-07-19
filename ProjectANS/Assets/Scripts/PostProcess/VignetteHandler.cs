5c67834499a0c0f004b64b455f25aa1528fad3b9
Sitra <yuzurusitra@gmail.com>
2024-07-19 11:07:30 +0900

    ポストプロセッシングとVignetteの値変更処理の実装

---
+++ ProjectANS/Assets/Scripts/PostProcess/VignetteHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace PostProcess
+{
+    public class VignetteHandler
+    {
+        private static VignetteHandler _instance;
+        public static VignetteHandler Instance => _instance ??= new VignetteHandler();
+        private readonly Vignette _vignette;
+
+        private VignetteHandler()
+        {
+            var postProcessVolume = GameObject.FindWithTag("PostprocessVolime").GetComponent<PostProcessVolume>();
+            // PostProcessVolumeからVignetteの設定を取得
+            if (postProcessVolume.profile.TryGetSettings(out _vignette))
+            {
+                _vignette.intensity.value = 0.0f;
+            }
+        }
+
+        public void SetVignetteIntensity(float intensity)
+        {
+            if (_vignette != null)
+            {
+                _vignette.intensity.value = intensity;
+            }
+        }
+    }
+}
