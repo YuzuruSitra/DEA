dd21f5b00a7f8933792cc25971d3d9f3855620ee
Sitra <yuzurusitra@gmail.com>
2024-09-25 18:14:08 +0900

    サイズ座標調整

--- ProjectANS/Assets/Scripts/System/Map/InRoomChecker.cs
+++ ProjectANS/Assets/Scripts/System/Map/InRoomChecker.cs
@@ -12,17 +12,14 @@ namespace System.Map
 
             for (var i = 0; i < _stageGenerator.RoomCount; i++)
             {
-                // 部屋�?�四隅の座標を取�?
                 var topLeftZ = roomInfo[i, (int)StageGenerator.RoomStatus.TopLeftZ];
                 var bottomLeftX = roomInfo[i, (int)StageGenerator.RoomStatus.BottomLeftX];
                 var bottomLeftZ = roomInfo[i, (int)StageGenerator.RoomStatus.BottomLeftZ];
                 var bottomRightX = roomInfo[i, (int)StageGenerator.RoomStatus.BottomRightX];
 
-                // プレイヤーの座標を整数に変換して�?囲�?にあるか判�?
                 var targetX = Mathf.FloorToInt(pos.x);
                 var targetZ = Mathf.FloorToInt(pos.z);
 
-                // �?囲�?にあるかど�?か�?�判�?
                 var isInRoom = targetX >= bottomLeftX && targetX <= bottomRightX &&
                                targetZ >= bottomLeftZ && targetZ <= topLeftZ;
 
