fb61aa72f3ddfa820316bd3f01fd97999abdc6d2
Sitra <yuzurusitra@gmail.com>
2024-10-26 21:25:38 +0900

    リザルト時のキャラ切り替え

--- ProjectDEA/Assets/Scripts/UI/ResultSceneHandler.cs
+++ ProjectDEA/Assets/Scripts/UI/ResultSceneHandler.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 
 namespace UI
 {
-    public class ResultPanelHandler : MonoBehaviour
+    public class ResultSceneHandler : MonoBehaviour
     {
         [SerializeField] private string[] _resultString;
         [SerializeField] private TextMeshProUGUI _resultText;
         [SerializeField] private TextMeshProUGUI _bornCountText;
-        
+        [SerializeField] private GameObject _clearObj;
+        [SerializeField] private GameObject _failedObj;
         private void Start()
         {
             var dungeonLayerHandler = GameObject.FindWithTag("DungeonLayerHandler").GetComponent<DungeonLayerHandler>();
             _resultText.text = dungeonLayerHandler.IsGameClear ? _resultString[0] : _resultString[1];
+            var target = dungeonLayerHandler.IsGameClear ? _clearObj : _failedObj;
+            target.SetActive(true);
             var inventoryHandler = GameObject.FindWithTag("InventoryHandler").GetComponent<InventoryHandler>();
             _bornCountText.text = "" + inventoryHandler.ItemSets[(int)ItemKind.Born]._count;
         }
