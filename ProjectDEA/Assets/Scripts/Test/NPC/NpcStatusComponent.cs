fabc82a9c5f2531bb18b36f6370884a563b1261a
Sitra <yuzurusitra@gmail.com>
2025-01-12 01:54:39 +0900

    スタミナと満腹度の計算処理を実装

--- ProjectDEA/Assets/Scripts/Test/NPC/NpcStatusComponent.cs
+++ ProjectDEA/Assets/Scripts/Test/NPC/NpcStatusComponent.cs
@@ -4,11 +4,18 @@ namespace Test.NPC
 {
     public class NpcStatusComponent : MonoBehaviour
     {
+        private const int CalcTiming = 1;
+        
+        [SerializeField] private float _staminaChangeSecond;
         [SerializeField] private float _maxStamina;
         public float CurrentStamina { get; private set; }
+        private float _staminaConsumeTimer;
+        private float _staminaRecoverTimer;
         
+        [SerializeField] private float _fullnessChangeSecond;
         [SerializeField] private float _maxFullness;
         public float CurrentFullness { get; private set; }
+        private float _fullnessConsumeTimer;
         
         private void Start()
         {
@@ -16,24 +23,34 @@ namespace Test.NPC
             CurrentFullness = _maxFullness;
         }
 
-        private void ConsumeStamina(float amount)
+        private void ConsumeStamina()
         {
-
-        }
-
-        private void RecoverStamina(float amount)
-        {
-
-        }
-
-	
-        public void GainStamina(float amount)
-        {
-            CurrentStamina += amount;
+            _staminaConsumeTimer += Time.deltaTime;
+            if (!(_staminaConsumeTimer >= CalcTiming)) return;
+            CurrentStamina -= _staminaChangeSecond;
             CurrentStamina = Mathf.Clamp(CurrentStamina, 0, _maxStamina);
+            _staminaConsumeTimer = 0f;
         }
-	
-        public void ChangeFullness(float amount)
+
+        private void RecoverStamina()
+        {
+            _staminaRecoverTimer += Time.deltaTime;
+            if (!(_staminaRecoverTimer >= CalcTiming)) return;
+            CurrentStamina += _staminaChangeSecond;
+            CurrentStamina = Mathf.Clamp(CurrentStamina, 0, _maxStamina);
+            _staminaRecoverTimer = 0f;
+        }
+        
+        private void ConsumeFullness()
+        {
+            _fullnessConsumeTimer += Time.deltaTime;
+            if (!(_fullnessConsumeTimer >= CalcTiming)) return;
+            CurrentFullness -= _fullnessChangeSecond;
+            CurrentFullness = Mathf.Clamp(CurrentFullness, 0, _maxFullness);
+            _fullnessConsumeTimer = 0f;
+        }
+        
+        public void AddFullness(float amount)
         {
             CurrentFullness += amount;
             CurrentFullness = Mathf.Clamp(CurrentFullness, 0, _maxFullness);
