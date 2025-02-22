c280f17e7d5d62b8e0f54db59249eac486f63865
Sitra <yuzurusitra@gmail.com>
2025-02-22 21:33:44 +0900

    デバッグ等のシーン遷移追加

--- ProjectDEA/Assets/Scripts/Test/DebugSceneChange.cs
+++ ProjectDEA/Assets/Scripts/Test/DebugSceneChange.cs
@@ -1,3 +1,4 @@
+using Manager;
 using UnityEngine;
 
 namespace Test
@@ -5,14 +6,15 @@ namespace Test
 	public class DebugSceneChange : MonoBehaviour
 	{
 		[SerializeField] private bool _isDebug;
-
+		[SerializeField] private DungeonLayerHandler _dungeonLayerHandler;
+		
 		// Update is called once per frame
 		private void Update()
 		{
 			if (!_isDebug) return;
 			if (Input.GetKeyDown(KeyCode.G))
 			{
-		    
+				_dungeonLayerHandler.NextDungeonLayer();
 			}
 		}
 	}
